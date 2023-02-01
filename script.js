ad513b32d29edddeaf617233875c0a2e1b26920b
Jay Kim <jaykim@Jays-MacBook-Pro.local>
2023-02-01 16:21:30 -0600

    Update App class

--- script.js
+++ script.js
@@ -11,66 +11,91 @@ const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
 
-// Global variable
-let map, mapEvent;
+class App {
+  #map;
+  #mapEvent;
 
-// Getting Geolocation
-if (navigator.geolocation)
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      // Distructring latitude and longitude to get data and apply it
-      const { latitude, longitude } = position.coords;
-      const coords = [latitude, longitude];
+  constructor() {
+    this._getPosition();
 
-      // Leaflet source to display current location on map
-      map = L.map("map").setView(coords, 12);
+    // After data submit, eventhandlings
+    form.addEventListener("submit", this._newWorkout.bind(this));
 
-      // Changed map theme from https:/tile.openstreetmap.org/{z}/{x}/{y}.png
-      L.tileLayer("https:/{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+    // When type is chaged, toggle from cadence to elevation input
+    inputType.addEventListener("change", this._toggleElevationField);
+  }
 
-      // Click on map to display input fields
-      map.on("click", function (mapE) {
-        mapEvent = mapE;
-        // Click on map and display the input field
-        form.classList.remove("hidden");
-        inputDistance.focus();
-      });
-    },
-    // Alert when current location is blocked
-    function () {
-      alert("Could not get your position");
-    }
-  );
+  _getPosition() {
+    // Getting Geolocation
+    if (navigator.geolocation)
+      navigator.geolocation.getCurrentPosition(
+        this._loadMap.bind(this),
+        // Alert when current location is blocked
+        function () {
+          alert("Could not get your position");
+        }
+      );
+  }
 
-// After data submit, display marker
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
+  _loadMap(position) {
+    // Distructring latitude and longitude to get data
+    const { latitude, longitude } = position.coords;
+    const coords = [latitude, longitude];
 
-  // Clear input fields
-  inputDistance.value =
-    inputDuration.value =
-    inputCadence.value =
-    inputElevation.value =
-      "";
+    // Leaflet source to display current location on map
+    this.#map = L.map("map").setView(coords, 12);
 
-  // Distructure map latitude and longitude
-  const { lat, lng } = mapEvent.latlng;
+    // Changed map theme from https:/tile.openstreetmap.org/{z}/{x}/{y}.png
+    L.tileLayer("https:/{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
 
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      // Popup box options
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: "running-popup",
-      })
-    )
-    .setPopupContent("Fit Zone")
-    .openPopup();
-});
+    // Click on map to show form
+    this.#map.on("click", this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    this.#mapEvent = mapE;
+    // Click on map and display the input field
+    form.classList.remove("hidden");
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+
+    // Clear input fields
+    inputDistance.value =
+      inputDuration.value =
+      inputCadence.value =
+      inputElevation.value =
+        "";
+
+    // Distructure map latitude and longitude
+    const { lat, lng } = this.#mapEvent.latlng;
+
+    // Display marker
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(
+        // Popup box options
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: "running-popup",
+        })
+      )
+      .setPopupContent("Fit Zone")
+      .openPopup();
+  }
+}
+
+const app = new App();
